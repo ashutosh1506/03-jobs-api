38f317c2168144800962f8094532325eebb62fb7
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-19 22:19:15 +0530

    Login Controller
    Completed

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -3,7 +3,7 @@ const { CustomAPIError } = require("../errors");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
-    return res.status(500).json({ msg: err.message });
+    return res.status(err.statusCode).json({ msg: err.message });
   }
   return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
 };
