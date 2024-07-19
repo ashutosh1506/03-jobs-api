bc78201db2a990526e9401ca7e5c0bae3228d1ec
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-19 21:24:35 +0530

    hash password and save user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,11 @@
+const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const { BadRequestError } = require("../errors");
+const bcrypt = require("bcryptjs");
+
 const register = async (req, res) => {
-  res.send("Register user");
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send("Login user");
