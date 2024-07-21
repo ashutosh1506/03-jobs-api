c04c2ad573becfef04f8e8540cdb2ae8b23aba79
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-21 13:24:00 +0530

    getJob Controller completed

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,12 +1,22 @@
 const { StatusCodes } = require("http-status-codes");
 const Job = require("../models/Job");
+const { NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
   const jobs = await Job.find({ createdBy: req.user.userID }).sort("createdAt");
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 const getJob = async (req, res) => {
-  res.send("Get a Job");
+  const {
+    user: { userID },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({ _id: jobId, createdBy: userID });
+  if (!job) {
+    throw new NotFoundError(`No Job Found with jobId : ${jobId} `);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
