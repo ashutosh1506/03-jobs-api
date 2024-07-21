52050b7d5b2305aa811b8d2c389c7accae21548a
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-21 14:11:28 +0530

    Errors handled
    Validation and Cast Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,4 @@
 const { StatusCodes } = require("http-status-codes");
-const { CustomAPIError } = require("../errors");
 
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -7,14 +6,24 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong! Try again later",
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
   }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate Values entered for ${Object.keys(
       err.keyValue
     )} field, Pls choose another value`;
     customError.statusCode = 400;
+  }
+
+  if (err.name === "CastError") {
+    customError.msg = `No item found with id : ${err.value}`;
+    customError.statusCode = 404;
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
